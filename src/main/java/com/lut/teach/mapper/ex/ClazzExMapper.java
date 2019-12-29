641423da81f527538ee71d267f1d59a944cbbbac
lirl <2849569929@qq.com>
2019-12-29 15:22:42 +0800

    新增

--- src/main/java/com/lut/teach/mapper/ex/ClazzExMapper.java
+++ src/main/java/com/lut/teach/mapper/ex/ClazzExMapper.java
@@ -6,9 +6,7 @@ import java.util.List;
 
 public interface ClazzExMapper {
     List<ClazzEx> selectAll();
-    List<ClazzEx> selectTitleOrAuthor(String word);
     List<ClazzEx> selectName(String word);
     List<ClazzEx> selectDescription(String word);
-
     List<ClazzEx> selectNameOrDescription(String word);
 }
