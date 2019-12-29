eb1ba2f8ec28d0cba3299cfeb8e78a7c05a95ec7
liuy <908657326@qq.com>
2019-12-29 10:01:09 +0800

    自动提交

--- src/main/java/com/lut/teach/TeachApplication.java
+++ src/main/java/com/lut/teach/TeachApplication.java
@@ -1,10 +1,11 @@
 package com.lut.teach;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
+@MapperScan("com.lut.teach.mapper")
 public class TeachApplication {
 
     public static void main(String[] args) {
