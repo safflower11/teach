e03f5333ca1e37c6b451413abdc59e1a064218f5
liuy <908657326@qq.com>
2019-12-31 09:32:55 +0800

    自动生成

---
+++ src/main/java/com/lut/teach/bean/Answer.java
@@ -0,0 +1,51 @@
+package com.lut.teach.bean;
+
+import java.io.Serializable;
+
+public class Answer implements Serializable {
+    private Integer id;
+
+    private String answer;
+
+    private Integer surveyId;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer == null ? null : answer.trim();
+    }
+
+    public Integer getSurveyId() {
+        return surveyId;
+    }
+
+    public void setSurveyId(Integer surveyId) {
+        this.surveyId = surveyId;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getClass().getSimpleName());
+        sb.append(" [");
+        sb.append("Hash = ").append(hashCode());
+        sb.append(", id=").append(id);
+        sb.append(", answer=").append(answer);
+        sb.append(", surveyId=").append(surveyId);
+        sb.append(", serialVersionUID=").append(serialVersionUID);
+        sb.append("]");
+        return sb.toString();
+    }
+}
