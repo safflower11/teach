8e45ba4e0b1019cd26d71de67c258b42c72e82c2
lirl <2849569929@qq.com>
2019-12-31 12:08:06 +0800

    xx

--- src/main/java/com/lut/teach/web/controller/RSurveyController.java
+++ src/main/java/com/lut/teach/web/controller/RSurveyController.java
@@ -3,6 +3,8 @@ package com.lut.teach.web.controller;
 import com.lut.teach.service.RSurveyService;
 import com.lut.teach.util.Message;
 import com.lut.teach.util.MessageUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Rsurvey")
+@Api(description = "课调管理")
 public class RSurveyController {
     @Autowired
     private RSurveyService rSurveyService;
+    @ApiOperation(value = "添加")
     @PostMapping("/add")
     public Message add(int departid,int classid,int courseid,int questionid,int teachid,int code){
         rSurveyService.insert(departid,classid,courseid,questionid,teachid,code);
@@ -20,6 +24,7 @@ public class RSurveyController {
         System.out.println(classid);
         return MessageUtil.success();
     }
+    @ApiOperation(value = "修改")
     @PostMapping("/update")
     public Message update(int id,int departid,int classid,int courseid,int questionid,int teachid,int code){
         rSurveyService.update(id,departid,classid,courseid,questionid,teachid,code);
